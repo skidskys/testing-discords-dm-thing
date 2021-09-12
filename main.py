f23ce23987c14ac1ca5c0334209c4bea76650a34
tax2trappy <81910405+skidskys@users.noreply.github.com>
2021-09-12 13:27:42 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# dont bother logging the token this is for expirementation LOl
+
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('discordK')
+
+client.login('NzUxOTE0OTc3MDMxNDg3NDk5.X1QBZA.IKUM-x9yysDOGKmODL79OTqX_CE')
